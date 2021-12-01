2754f4abc428c0ff025dc3d54012e59be7df7b29
EC2 Default User <ec2-user@ip-172-31-38-0.ap-southeast-1.compute.internal>
2021-12-01 16:27:20 +0000

    modify .tf files

---
+++ locals.tf
@@ -0,0 +1,7 @@
+locals {
+  vpc_id           = "vpc-0163585345c5eaec9"
+  subnet_id        = "subnet-045863fc5ed7f2a91"
+  ssh_user         = "ubuntu"
+  key_name         = "aws_ec2"
+  private_key_path = "aws_ec2.pem"
+}
--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
-locals {
-  vpc_id           = "vpc-0163585345c5eaec9"
-  subnet_id        = "subnet-045863fc5ed7f2a91"
-  ssh_user         = "ubuntu"
-  key_name         = "aws_ec2"
-  private_key_path = "aws_ec2.pem"
-}
-
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.67.0"
+    }
+  }
+  
   backend "remote" {
     organization = "htunnthuthu"
 
